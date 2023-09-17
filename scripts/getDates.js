917baa7d3a82c13237404ec151b4a9911093e663
bottles10 <henryaovare@gmail.com>
2023-09-17 22:51:40 +0000

    Add javascript for date update

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+let d = new Date()
+let yearNow = document.querySelector("#year")
+yearNow.textContent = d.getFullYear() 
+let oLastModif = new Date(document.lastModified)
+let pageLastModified = document.querySelector("#lastModified")
+pageLastModified.textContent = oLastModif
