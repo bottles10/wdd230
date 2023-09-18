f46e4c2a0abe7ac69d0681c8b91c30543d5fe5d8
bottles10 <henryaovare@gmail.com>
2023-09-18 04:16:50 +0000

    formatted codes

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,6 +1,7 @@
 let d = new Date()
 let yearNow = document.querySelector("#year")
-yearNow.textContent = d.getFullYear() 
+yearNow.textContent = d.getFullYear()
+
 let oLastModif = new Date(document.lastModified)
 let pageLastModified = document.querySelector("#lastModified")
 pageLastModified.textContent = oLastModif
